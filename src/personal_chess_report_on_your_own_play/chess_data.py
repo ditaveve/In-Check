828da87127b19dc6b23b3e4aab686607a68e1c8f
Diana Angheluta <dianaangheluta@Dianas-MacBook-Pro.local>
2026-08-24 15:39:13 +0100

    get all available game archives of a single player

--- src/personal_chess_report_on_your_own_play/chess_data.py
+++ src/personal_chess_report_on_your_own_play/chess_data.py
@@ -2,18 +2,27 @@ import requests
 import os
 from dotenv import load_dotenv
 
+BASE_URL="https://api.chess.com/pub/player"
 
 load_dotenv()
 HEADERS = {
     "User-Agent": f"personal-chess-report-on-your-own-play (contact: {os.getenv("CONTACT_EMAIL")})"
 }
 
-def get_monthly_games(username, YYYY, MM):
-    url = f"https://api.chess.com/pub/player/{username}/games/{YYYY}/{MM:0>2}"
-    response = requests.get(url, headers=HEADERS)
-    print(response.status_code)
+def get_available_archives(username):
+    all_archives_url = f"{BASE_URL}/{username}/games/archives"
+    response = requests.get(all_archives_url, headers=HEADERS)
     if response.status_code == 200:
         data = response.json()
         return data
     else:
-        print("Failed to retrieve data.")
+        print("Failed to retrieve all archives.")
+
+def get_monthly_games(username, YYYY, MM):
+    monthly_arhive_url = f"{BASE_URL}/{username}/games/{YYYY}/{MM}"
+    response = requests.get(monthly_arhive_url, headers=HEADERS)
+    if response.status_code == 200:
+        data = response.json()
+        return data
+    else:
+        print("Failed to retrieve monthly games.")
