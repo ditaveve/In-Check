d6d9ed66e37c51e194857d8e8a1588fcfe175964
Diana Angheluta <dianaangheluta@Dianas-MacBook-Pro.local>
2026-08-24 16:34:54 +0100

    handle http errors

--- src/personal_chess_report_on_your_own_play/chess_data.py
+++ src/personal_chess_report_on_your_own_play/chess_data.py
@@ -12,19 +12,15 @@ HEADERS = {
 def get_available_archives(username):
     all_archives_url = f"{BASE_URL}/{username}/games/archives"
     response = requests.get(all_archives_url, headers=HEADERS)
-    if response.status_code == 200:
-        data = response.json()
-        return data['archives']
-    else:
-        print("Failed to retrieve all archives.")
+    response.raise_for_status()
+    data = response.json()
+    return data['archives']
 
 def get_monthly_games(monthly_arhive_url):
     response = requests.get(monthly_arhive_url, headers=HEADERS)
-    if response.status_code == 200:
-        data = response.json()
-        return data
-    else:
-        print("Failed to retrieve monthly games.")
+    response.raise_for_status()
+    data = response.json()
+    return data
 
 def get_game_history(username, how_many_games):
     all_archives = get_available_archives(username)[::-1]
@@ -33,26 +29,22 @@ def get_game_history(username, how_many_games):
         for monthly_archive_url in all_archives:
             monthly_archives = get_monthly_games(monthly_archive_url)['games']
             for game in monthly_archives:
-                print(game)
+                all_games.append(game)
                 how_many_games -= 1
                 if how_many_games <= 0:
-                    return
+                    return all_games
     return all_games
 
 def get_user_profile(username):
     profile_url = f"{BASE_URL}/{username}"
     response = requests.get(profile_url, headers=HEADERS)
-    if response.status_code == 200:
-        data = response.json()
-        return data
-    else:
-        print("Failed to retrieve user profile.")
+    response.raise_for_status()
+    data = response.json()
+    return data
 
 def get_user_stats(username):
     stats_url = f"{BASE_URL}/{username}/stats"
     response = requests.get(stats_url, headers=HEADERS)
-    if response.status_code == 200:
-        data = response.json()
-        return data
-    else:
-        print("Failed to retrieve user stats.")
+    response.raise_for_status()
+    data = response.json()
+    return data
